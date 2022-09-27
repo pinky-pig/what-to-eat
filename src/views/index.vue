2b6cf75b937df1f3d7017302ca1739992624809c
pinky-pig <2545589993@qq.com>
2022-09-27 15:24:00 +0800

    init repo

---
+++ src/views/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class="text-3xl text-green-300"> Vite Vue3 Project</div>
+  </div>
+</template>
+<script setup lang="ts">
+    
+</script>
