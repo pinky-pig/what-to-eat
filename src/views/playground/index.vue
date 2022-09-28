7a5fb6d39d164753713b4ad3de197fde90253d2e
pinky-pig <2545589993@qq.com>
2022-09-28 15:26:38 +0800

    feat: add bg

--- src/views/playground/index.vue
+++ src/views/playground/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="w-4/5 h-3/5 z-20 rounded-xl">
-    <img class="w-full h-full object-contain" src="@/assets/theme/theme-2.jpg" alt="">
+  <div class="w-4/5 h-3/5 z-20 rounded-xl bg-blue-200/5 relative">
+    <!-- <img class="w-full h-full object-contain" src="@/assets/theme/one/背景.png" alt=""> -->
+
+    <img class="absolute top-0 w-full z-10" src="@/assets/theme/one/header.png" />
+    
+    <svg class="w-full h-full absolute" >
+      <defs>
+        <pattern id="rect" patternUnits="userSpaceOnUse" width="15" height="19">
+          <rect width="12" height="16" fill="#092B17" rx="2"></rect>
+        </pattern>
+      </defs>
+
+      <rect id="canvas" width="100%" height="100%" fill="#345B41" />
+      <rect id="canvas" width="100%" height="100%" fill="url(#rect)" />
+    </svg>
+
+    <img class="absolute bottom-0 w-full z-10" src="@/assets/theme/one/foot.png" />
+
   </div>
 </template>
 <script setup lang="ts"> 
